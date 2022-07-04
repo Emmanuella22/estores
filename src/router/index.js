920621e52d6eb8c04bc1618b5d11cda9bff832a5
Emmanuella Onedibe <emmanuellaonedibe@gmail.com>
2022-07-04 10:33:45 +0100

    Es

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "../pages";
-
-
+import {ProductLists } from "../pages";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        
+        <Route path="/products" element={<ProductLists />} />
       </Routes>
     </BrowserRouter>
   );
